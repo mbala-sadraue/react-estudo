bd47e0c2eccda635f81fc0471601127908fc1427
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-08-19 16:16:30 +0100

    filtrar produto

--- react-estudo/src/estudo/FilterableProductTable.jsx
+++ react-estudo/src/estudo/FilterableProductTable.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 
@@ -12,34 +13,52 @@ function FilterableProdutTable() {
         { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
     ]
 
+    const [filterText, setFilterText] = useState('procurar 2')
+    const [inStockOnly, setInStockOnly] = useState(false)
+    function onFilterTextChange(value) {
+        setFilterText(value)
+        console.log(value);
+        
+    }
+    return (
+        <>
+            <SearchBar filterText={filterText} inStockOnly={inStockOnly} onFilterTextChange={onFilterTextChange} onInStockOnlyChange={setInStockOnly} />
+            <ProductTable products={products} filterText={filterText}  inStockOnly={inStockOnly}/>
+        </>)
+
+
+
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
     let rows = []
     let lastCategory = null;
 
 
-
+    console.log(filterText);
+    
 
     products.forEach(
         (product) => {
 
-            console.log(product.name);
-
-
+            if (product.name.toLowerCase().indexOf(
+                filterText.toLowerCase()
+            ) === - 1) {
+                return;
+            }
+            if (inStockOnly && !product.stocked) {
+                return;
+            }
             if (product.category !== lastCategory) {
                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />)
             }
-
             rows.push(<ProductRow
                 product={product}
                 key={product.name} />)
-
             lastCategory = product.category;
 
         }
     )
-    console.log(rows);
-
-
-
     return (<>
         <h3>Lsita de Produtos</h3>
         <div>
@@ -60,7 +79,7 @@ function FilterableProdutTable() {
 }
 
 
-function ProductCategoryRow({category}) {
+function ProductCategoryRow({ category }) {
 
     return (
         <tr>
@@ -71,9 +90,9 @@ function ProductCategoryRow({category}) {
 }
 
 
-function ProductRow({product}) {
+function ProductRow({ product }) {
 
-    let name =  product.stocked ?product.name: <span style={{ color: 'red' }}>
+    let name = product.stocked ? product.name : <span style={{ color: 'red' }}>
         {product.name}
     </span>;
 
@@ -83,8 +102,27 @@ function ProductRow({product}) {
     </tr>)
 }
 
+
+function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) {
+    return (
+        <form>
+            <input type="text" placeholder="Search..." value={filterText} onChange={(e) => onFilterTextChange(e.target.value)} />
+            <label>
+                <input type="checkbox" checked={inStockOnly} onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
+
+
 export default FilterableProdutTable;
 
+// Saber a divisao de componte
+// implementar de forma estatica
+//  Encontrar as variavel ou parte que são state
+//  Componente responsavel por alterar state
 
 
 
