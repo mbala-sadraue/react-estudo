1a968ecc599c6cdbd174ef7f3ccc0d943f8058d6
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-09-01 11:24:11 +0100

    treinar margine padding de tailwind

---
+++ react-estudo/src/tailwind/Inicio.jsx
@@ -0,0 +1,21 @@
+/*
+* Margin  e padding
+*
+* <div class="">
+
+  <h1 class="text-blue-900 font-bold hover:text-red-800">Mbala Sadraque</h1>
+
+<div class="w-52 bg-red-700 h-52 pt-5 pl-5">
+
+    <div class="h-40 w-40 bg-blue-500 py-1">
+
+      <div class="h-30 w-30 bg-green-400  mt-5 ml-5">
+      </div>
+    </div>
+</div>
+</div>
+*
+*
+*
+*
+* */
