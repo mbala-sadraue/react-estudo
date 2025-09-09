1192038120f19d2efe2bad155265ff681db4e5c4
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-09-09 13:58:28 +0100

    criar

--- ecommerce-x/src/pages/home/Home.tsx
+++ ecommerce-x/src/pages/home/Home.tsx
@@ -6,160 +6,172 @@ import { RiNumber1, RiNumber2, RiNumber3, RiNumber4, RiNumber5 } from "react-ico
 
 const Home = () => {
     return (
+        <main>
+            <section className="max-w-screen-2xl mx-auto sm:py-3 sm:px-4 lg:pt-10  md:px-0">
+                <div className="grid md:grid-cols-4 gap-4">
+                    <div className="bg-white pb-5">
 
-        <section className="max-w-screen-2xl mx-auto sm:py-3 sm:px-4 py-10  md:px-0">
+                        <ul>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4  text-gray-500 border-b border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <IoMdPhonePortrait className="text-xl" />
+                                        <span className="text-sm">Mobile</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4  text-gray-500 border-b border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <FaComputer className="text-xl" />
+                                        <span className="text-sm">Computers</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4 text-gray-500 border-b border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <IoCameraOutline className="text-xl" />
+                                        <span className="text-sm">Video & Cameras</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4 text-gray-500 border-b border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <FaTv className="text-xl" />
+                                        <span className="text-sm">Televisions</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4 text-gray-500 border-b border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <PiBicycleLight className="text-xl" />
+                                        <span className="text-sm">Bicycles</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4 text-gray-500 border-b border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <IoIosFitness className="text-xl" />
+                                        <span className="text-sm">Fitness</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4  border-b  border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <IoGameControllerOutline className="text-xl" />
+                                        <span className="text-sm">Mobile</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4  text-gray-500 border-b border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <IoMdPhonePortrait className="text-xl" />
+                                        <span className="text-sm">Mobile</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-between px-3 py-4">
+                                    <div className="flex items-center space-x-3">
+                                        <FaComputer className="text-xl" />
+                                        <span className="text-sm">Computers</span>
+                                    </div>
+                                    <IoMdArrowDropright />
+                                </a>
+                            </li>
+                        </ul>
 
-            <div className="grid md:grid-cols-4 gap-4">
-                <div className="bg-white pb-5">
+                    </div>
+                    <div className="bg-blue-950 md:col-span-2"></div>
+                    <div className="bg-white p-3 px-10">
+                        <div>
+                            <h2 className="font-semibold text-lg mb-3">Deal of the Day</h2>
+                            <div className="">
+                                <div className="text-sm flex justify-between items-center border-b border-gray-300 pb-3 my-4">
+                                    <div > <RiNumber1 className="text-2xl text-gray-400" /></div>
+                                    <div>
+                                        <h4 className="text-xs">Product Name</h4>
+                                        <p className="text-black font-semibold mt-1">$399.00</p>
+                                    </div>
+                                    <div className="w-15 h-15 rounded-lg bg-gray-300">
 
-                    <ul>
-                        <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4  text-gray-500 border-b border-gray-300">
-                                <div className="flex items-center space-x-3">
-                                    <IoMdPhonePortrait className="text-xl" />
-                                    <span className="text-sm">Mobile</span>
+                                    </div>
                                 </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4  text-gray-500 border-b border-gray-300">
-                                <div className="flex items-center space-x-3">
-                                    <FaComputer className="text-xl" />
-                                    <span className="text-sm">Computers</span>
-                                </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4 text-gray-500 border-b border-gray-300">
-                                <div className="flex items-center space-x-3">
-                                    <IoCameraOutline className="text-xl" />
-                                    <span className="text-sm">Video & Cameras</span>
-                                </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4 text-gray-500 border-b border-gray-300">
-                                <div className="flex items-center space-x-3">
-                                    <FaTv className="text-xl" />
-                                    <span className="text-sm">Televisions</span>
-                                </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4 text-gray-500 border-b border-gray-300">
-                                <div className="flex items-center space-x-3">
-                                    <PiBicycleLight className="text-xl" />
-                                    <span className="text-sm">Bicycles</span>
-                                </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4 text-gray-500 border-b border-gray-300">
-                                <div className="flex items-center space-x-3">
-                                    <IoIosFitness className="text-xl" />
-                                    <span className="text-sm">Fitness</span>
-                                </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4  border-b  border-gray-300">
-                                <div className="flex items-center space-x-3">
-                                    <IoGameControllerOutline className="text-xl" />
-                                    <span className="text-sm">Mobile</span>
-                                </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                         <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4  text-gray-500 border-b border-gray-300">
-                                <div className="flex items-center space-x-3">
-                                    <IoMdPhonePortrait className="text-xl" />
-                                    <span className="text-sm">Mobile</span>
-                                </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-between px-3 py-4">
-                                <div className="flex items-center space-x-3">
-                                    <FaComputer className="text-xl" />
-                                    <span className="text-sm">Computers</span>
-                                </div>
-                                <IoMdArrowDropright />
-                            </a>
-                        </li>
-                    </ul>
+                                <div className="text-sm flex justify-between items-center border-b border-gray-300 pb-3 my-4">
+                                    <div > <RiNumber2 className="text-2xl text-gray-400" /></div>
+                                    <div>
+                                        <h4 className="text-xs">Product Name</h4>
+                                        <p className="text-black font-semibold mt-1">$399.00</p>
+                                    </div>
+                                    <div className="w-15 h-15 rounded-lg bg-gray-300">
 
-                </div>
-                <div className="bg-blue-950 md:col-span-2"></div>
-                <div className="bg-white p-3 px-10">
-                    <div>
-                        <h2 className="font-semibold text-lg mb-3">Deal of the Day</h2>
-                        <div className="">
-                            <div className="text-sm flex justify-between items-center border-b border-gray-300 pb-3 my-4">
-                                <div > <RiNumber1  className="text-2xl text-gray-400"/></div>
-                                <div>
-                                    <h4 className="text-xs">Product Name</h4>
-                                    <p className="text-black font-semibold mt-1">$399.00</p>
+                                    </div>
                                 </div>
-                                <div className="w-15 h-15 rounded-lg bg-gray-300">
-                                    
-                                </div>
-                            </div>
-                             <div className="text-sm flex justify-between items-center border-b border-gray-300 pb-3 my-4">
-                                <div > <RiNumber2  className="text-2xl text-gray-400"/></div>
-                                <div>
-                                    <h4 className="text-xs">Product Name</h4>
-                                    <p className="text-black font-semibold mt-1">$399.00</p>
-                                </div>
-                                <div className="w-15 h-15 rounded-lg bg-gray-300">
-                                    
-                                </div>
-                            </div>
-                             <div className="text-sm flex justify-between items-center border-b border-gray-300 pb-3 my-4">
-                                <div > <RiNumber3 className="text-2xl text-gray-400"/></div>
-                                <div>
-                                    <h4 className="text-xs">Product Name</h4>
-                                    <p className="text-black font-semibold mt-1">$399.00</p>
-                                </div>
-                                <div className="w-15 h-15 rounded-lg bg-gray-300">
-                                    
-                                </div>
-                            </div>
-                             <div className="text-sm flex justify-between items-center border-b border-gray-300 pb-3 my-4">
-                                <div > <RiNumber4  className="text-2xl text-gray-400"/></div>
-                                <div>
-                                    <h4 className="text-xs">Product Name</h4>
-                                    <p className="text-black font-semibold mt-1">$399.00</p>
-                                </div>
-                                <div className="w-15 h-15 rounded-lg bg-gray-300">
-                                    
-                                </div>
-                            </div>
-                             <div className="text-sm flex justify-between items-center pb-3">
-                                <div > <RiNumber5  className="text-2xl text-gray-400"/></div>
-                                <div>
-                                    <h4 className="text-xs">Product Name</h4>
-                                    <p className="text-black font-semibold mt-1">$399.00</p>
-                                </div>
-                                <div className="w-15 h-15 rounded-lg bg-gray-300">
-                                    
-                                </div>
-                            </div>
+                                <div className="text-sm flex justify-between items-center border-b border-gray-300 pb-3 my-4">
+                                    <div > <RiNumber3 className="text-2xl text-gray-400" /></div>
+                                    <div>
+                                        <h4 className="text-xs">Product Name</h4>
+                                        <p className="text-black font-semibold mt-1">$399.00</p>
+                                    </div>
+                                    <div className="w-15 h-15 rounded-lg bg-gray-300">
 
+                                    </div>
+                                </div>
+                                <div className="text-sm flex justify-between items-center border-b border-gray-300 pb-3 my-4">
+                                    <div > <RiNumber4 className="text-2xl text-gray-400" /></div>
+                                    <div>
+                                        <h4 className="text-xs">Product Name</h4>
+                                        <p className="text-black font-semibold mt-1">$399.00</p>
+                                    </div>
+                                    <div className="w-15 h-15 rounded-lg bg-gray-300">
+
+                                    </div>
+                                </div>
+                                <div className="text-sm flex justify-between items-center pb-3">
+                                    <div > <RiNumber5 className="text-2xl text-gray-400" /></div>
+                                    <div>
+                                        <h4 className="text-xs">Product Name</h4>
+                                        <p className="text-black font-semibold mt-1">$399.00</p>
+                                    </div>
+                                    <div className="w-15 h-15 rounded-lg bg-gray-300">
+
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
 
-        </section>
+            </section>
+
+            <section className="max-w-screen-2xl mx-auto sm:py-3 sm:px-4  lg:px-0">
+            
+                <div className="flex">
+                    <div className="flex overflow-x-auto scrollbar-hidden snap-x snap-mandatory gap-4 w-full   rounded-lg  lg:overflow-x-hidden">
+                    <div className="bg-white text-lg font-bold min-w-[270px] h-50 md:flex-1 snap-start rounded-sm">Item 1</div>
+                    <div className="bg-white p-4 text-lg font-bold min-w-[270px] md:flex-1 snap-start rounded-sm">Item 2</div>
+                    <div className="bg-white p-4 text-lg font-bold min-w-[270px] md:flex-1 snap-start rounded-sm">Item 3</div>
+                    <div className="bg-white  p-4 text-lg font-bold min-w-[270px] md:flex-1 snap-start rounded-sm">Item 4</div>
+                </div>
+                </div>
+            </section>
+        </main>
     )
 }
 
