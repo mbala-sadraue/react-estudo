933c2fa12c08764fc9e7dff50bb8abf3aba14e18
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-09-11 13:44:18 +0100

    criar card de curso big

--- ecommerce-x/src/pages/home/Home.tsx
+++ ecommerce-x/src/pages/home/Home.tsx
@@ -8,6 +8,7 @@ import { RiNumber1, RiNumber2, RiNumber3, RiNumber4, RiNumber5 } from "react-ico
 import { TfiReload } from "react-icons/tfi"
 
 const Home = () => {
+    const products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     return (
         <main>
             <section className="max-w-screen-2xl mx-auto py-3 sm:px-4 lg:pt-10  md:px-0">
@@ -185,44 +186,52 @@ const Home = () => {
                 </div>
                 <div className="flex">
                     <div className="flex overflow-x-auto scrollbar-hidden snap-x snap-mandatory gap-4 w-full   rounded-lg  lg:overflow-x-hidden">
-                        <div className="bg-white text-lg font-bold   snap-start rounded-sm p-2">
-                            <div className="bg-gray-100 min-w-[280px] min-h-[280px] relative">
-                                <div className="bg-blue-100 absolute right-2 top-2 py-2 px-3 rounded-md border border-gray-400">
-                                    <p className="text-xs font-medium text-blue-700">New</p>
-                                </div>
-                            </div>
-                            <div className="mt-2">
-                                <h4 className="text-sm">Product Name</h4>
-                                <p className="text-black font-semibold mt-1 text-sm">$399.00</p>
-                            </div>
-                            <div className="mt-1 border-t border-gray-300 pt-2 text-xs text-gray-500">
-                                <div className="flex">
-                                    <input type="number" className="border border-gray-300 rounded-md p-1 w-16 focus-visible:border-red-500" name="quantity" id="" minLength={1} min={0} />
-                                    <button className=" flex-1 py-1 rounded-md ml-3 flex items-center hover:text-blue-500">
-                                        <MdAddShoppingCart className="mr-2 text-xl" />
-                                        Add to Cart
-                                    </button>
-                                    <div className="flex space-x-3 justify-between items-center py-1">
-                                        <CiHeart className="text-xl" />
-                                        <TfiReload className="text-xl" />
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="bg-white p-4 text-lg font-bold min-w-[270px] snap-start rounded-sm">Item 2</div>
-                        <div className="bg-white p-4 text-lg font-bold min-w-[270px]  snap-start rounded-sm">Item 3</div>
-                        <div className="bg-white  p-4 text-lg font-bold min-w-[270px]  snap-start rounded-sm">Item 4</div>
-                        <div className="bg-white  p-4 text-lg font-bold min-w-[270px]  snap-start rounded-sm">Item 4</div>
-                        <div className="bg-white  p-4 text-lg font-bold min-w-[270px]  snap-start rounded-sm">Item 4</div>
-                        <div className="bg-white  p-4 text-lg font-bold min-w-[270px]  snap-start rounded-sm">Item 4</div>
+                        <ProductCardBig />
+                        <ProductCardBig />
+                        <ProductCardBig />
+                        <ProductCardBig />
+                        <ProductCardBig />
+                        <ProductCardBig />
+                        <ProductCardBig />
                     </div>
                 </div>
+            <section className="h-40 my-10 px-3 bg-white rounded-md"></section>
             </section>
+
 
 
         </main>
     )
 }
 
+const ProductCardBig = () => {
+
+    return (
+        <div className="bg-white text-lg font-bold   snap-start rounded-sm p-2">
+            <div className="bg-gray-100 min-w-[280px] min-h-[280px] relative">
+                <div className="bg-blue-100 absolute right-2 top-2 py-2 px-3 rounded-md border border-gray-400">
+                    <p className="text-xs font-medium text-blue-700">New</p>
+                </div>
+            </div>
+            <div className="mt-2">
+                <h4 className="text-sm">Product Name</h4>
+                <p className="text-black font-semibold mt-1 text-sm">$399.00</p>
+            </div>
+            <div className="mt-1 border-t border-gray-300 pt-2 text-xs text-gray-500">
+                <div className="flex">
+                    <input type="number" className="border border-gray-300 rounded-md p-1 w-16 focus-visible:border-red-500" name="quantity" id="" minLength={1} min={0} />
+                    <button className=" flex-1 py-1 rounded-md ml-3 flex items-center hover:text-blue-500">
+                        <MdAddShoppingCart className="mr-2 text-xl" />
+                        Add to Cart
+                    </button>
+                    <div className="flex space-x-3 justify-between items-center py-1">
+                        <CiHeart className="text-xl" />
+                        <TfiReload className="text-xl" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
 export default Home
