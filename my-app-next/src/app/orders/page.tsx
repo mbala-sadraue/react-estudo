96e0ead6fac89402ab047fefd9c3b0678eefafa1
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-09-29 15:48:59 +0100

    criar component dinamico

--- my-app-next/src/app/orders/page.tsx
+++ my-app-next/src/app/orders/page.tsx
@@ -1,4 +1,8 @@
+
+'use client'
 import { Metadata } from "next";
+import  Link  from "next/link";
+import React, { use, useEffect } from "react";
 
 export interface Product {
     id: number;
@@ -11,34 +15,67 @@ export interface Product {
 interface ProdutsResponse {
     products: Product[];
  }
-export const  metadata:Metadata ={
-title:'order page',
-description:'pagina de pedidos',
+// export const  metadata:Metadata ={
+// title:'order page',
+// description:'pagina de pedidos',
 
-}
-const OrderPage = async ()  =>{
+// }
+const OrdersPage = () => {
+'use client'
+    const [products, setProducts] = React.useState<Product[]>([]);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('https://dummyjson.com/products');
+            const data: ProdutsResponse = await response.json();
+            setProducts(data.products);
+        };
+        fetchProducts();
+    }, []);
 
-    const response = await fetch('https://dummyjson.com/products');
-    const data: ProdutsResponse = await response.json();
-
-
-    const produtos = data.products.map((product) => (
-        <div key={product.id}>
-                <img src={product.thumbnail} alt="" />
-                <h2>{product.title}</h2>
-                <p>{product.description}</p>
-                <p>Price: ${product.price.toString()}</p>
-        </div> )
-         )
     return (
-    <div className="max-w-7xl m-auto">
-        <h1>Order Page</h1>
-        <div className="grid grid-cols-3 gap-4">
-            {produtos}
+        <div>
+            <h1>Orders Page</h1>
+            <div className="grid grid-cols-3 gap-4">
+            {products.map((product) => (
+                <div key={product.id} className="border p-4">
+                    <Link href={`/orders/${product.id}`}>
+                        <h2 className="font-bold">{product.title}</h2>
+                        <p>{product.description}</p>
+                        <p>Price: ${product.price.toString()}</p>
+                        <img src={product.thumbnail} alt={product.title} />
+                    </Link>
+                </div>
+            ))}
+            </div>
         </div>
+    )
 
-
-    </div>)
 }
+// const OrderPage = async ()  =>{
 
-export default OrderPage;
+//     const response = await fetch('https://dummyjson.com/products');
+//     const data: ProdutsResponse = await response.json();
+
+
+//     const produtos = data.products.map((product) => (
+//         <div key={product.id}>
+     
+//             <h2 className="font-bold">{product.title}</h2>
+//             <p>{product.description}</p>
+//             <p>Price: ${product.price.toString()}</p>
+//             <img src={product.thumbnail} alt={product.title} />
+     
+//         </div> )
+//          )
+//     return (
+//     <div className="max-w-7xl m-auto">
+//         <h1>Order Page</h1>
+//         <div className="grid grid-cols-3 gap-4">
+//             {produtos}
+//         </div>
+
+
+//     </div>)
+// }
+
+export default OrdersPage;
