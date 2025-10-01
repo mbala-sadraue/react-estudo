60744587ef6e6ae7acae37988bdf73bf75efbc25
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-10-01 08:44:32 +0100

    criar types

---
+++ dashboard-admin/src/types/index.ts
@@ -0,0 +1,46 @@
+export interface Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+}
+
+export interface User {
+
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    username: string;
+    image: string;
+    address: {
+        address: string;
+        city: string;
+        state: string;
+        postalCode: string;
+    };
+    company: {
+        name: string;
+        title: string;
+    };
+}
+
+export interface AuthUser {
+    id: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    image: string;
+    token: string;
+    refreshToken: string;
+}
